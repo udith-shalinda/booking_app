44f4016e78a5dade907b805d6c4ae43a2f414f8d
Udith <udithshalinda2@gmail.com>
2019-06-01 19:51:10 +0530

    feed added

--- lib/ui/feed.dart
+++ lib/ui/feed.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+import '../modle/bookModle.dart';
 
 class ChallengeFeed extends StatefulWidget {
   @override
@@ -6,12 +10,27 @@ class ChallengeFeed extends StatefulWidget {
 }
 
 class _ChallengeFeedState extends State<ChallengeFeed> {
+
+  List<BookModle> bookedDatesList = List();
+  //BookModle bookModle;
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  DatabaseReference databaseReference;
+
+
+  @override
+  void initState() {
+    super.initState();
+    databaseReference = database.reference().child("bookedTimes");
+    databaseReference.onChildAdded.listen(_OnEntryAdded);
+
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.greenAccent,
-        title: new Text("Schedule of Date"),
+        title: new Text("News feed"),
         centerTitle: true,
       ),
       body: new Stack(
@@ -26,11 +45,40 @@ class _ChallengeFeedState extends State<ChallengeFeed> {
           ),
           new ListView(
             children: <Widget>[
-              new Text("hello there")
+              new Container(
+                height: 600,
+                child: new FirebaseAnimatedList(
+                    query: databaseReference,
+                    itemBuilder: (_, DataSnapshot snapshot,Animation<double> animation , int index){
+                      return new Card(
+                        child: new ListTile(
+//                            leading: CircleAvatar(
+//                              backgroundColor: Colors.redAccent,
+//                            ),
+                          title:  Text(snapshot.value['dateTime'].toString()),
+                          subtitle:  Text("Morning :  ${snapshot.value['morning'].toString()}  ${snapshot.value['morningPlayer'].toString()} "
+                              "\n Evening :  ${snapshot.value['evening'].toString() } ${snapshot.value['eveningPlayer'].toString()}"
+                              "\n Night :  ${snapshot.value['night'].toString()} ${snapshot.value['nightPlayer'].toString()}"),
+                          onTap: (){
+                            debugPrint(snapshot.value['key'].toString());
+                          },
+                        ),
+                      );
+                    }
+                ),
+              ),
+
             ],
           )
         ]
       )
     );
+  }
+
+
+  void _OnEntryAdded(Event event) {
+    setState(() {
+      bookedDatesList.add(BookModle.fromSnapshot(event.snapshot));
+    });
   }
 }
