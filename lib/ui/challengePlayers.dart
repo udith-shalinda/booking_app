9df55d307a6eb9be01b6215f858f1b31afac882c
Udith <udithshalinda2@gmail.com>
2019-06-14 21:21:23 +0530

    Theme added to the showPlayers

--- lib/ui/challengePlayers.dart
+++ lib/ui/challengePlayers.dart
@@ -12,27 +12,30 @@ class _ChallengePlayersState extends State<ChallengePlayers> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
       appBar: AppBar(
-        backgroundColor: Colors.greenAccent,
-        title: new Text("Schedule of Date"),
+        backgroundColor: Color.fromRGBO(52, 66, 86, 1.0),
+        title: new Text("Players"),
         centerTitle: true,
       ),
       body : new Stack(
         children: <Widget>[
-        new Center(
-          child: new Image.asset(
-          'images/cover_one.jpg',
-          fit: BoxFit.cover,
-          width: 500,
-          height: 1000,
-          ),
-        ),
         new ListView.builder(
           itemCount: widget.players.length,
           itemBuilder: (BuildContext context,int index){
             return new Card(
-              child: ListTile(
-                title: Text(widget.players[index]),
+              elevation: 8.0,
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: Container(
+                decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                child: ListTile(
+                  title: Text(
+                      widget.players[index],
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                  ),
+                ),
               ),
             );
           },
